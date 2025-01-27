2d9ad88772293ec3013dcc9ebae19f1d59fac956
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2025-01-27 21:29:23 +0300

    fixed(template): сократить время кеширования и убрать кеширование главной страницы со списком рассылок

--- mailmaster/urls.py
+++ mailmaster/urls.py
@@ -13,21 +13,21 @@ from mailmaster.views import (MessageCreateView, MessageDeleteView,
 app_name = MailmasterConfig.name
 
 urlpatterns = [
-    path("", cache_page(60)(NewsLetterListView.as_view()), name="index"),
+    path("", (NewsLetterListView.as_view()), name="index"),
     path("contact/", contact, name="contact"),
     path("view/<int:pk>/", NewsLetterDetailView.as_view(), name="view_newsletter"),
     path("create/", NewsLetterCreateView.as_view(), name="create_newsletter"),
     path("edit/<int:pk>/", NewsLetterUpdateView.as_view(), name="update_newsletter"),
     path("delete/<int:pk>/", NewsLetterDeleteView.as_view(), name="delete_newsletter"),
 
-    path("message/", cache_page(60)(MessageListView.as_view()), name="message_list"),
+    path("message/", cache_page(5)(MessageListView.as_view()), name="message_list"),
     path("create/message/", MessageCreateView.as_view(), name="create_message"),
     path("message/view/<int:pk>/", MessageDetailView.as_view(), name="view_message"),
     path("message/edit/<int:pk>/", MessageUpdateView.as_view(), name="update_message"),
     path(
         "message/delete/<int:pk>/", MessageDeleteView.as_view(), name="delete_message"
     ),
-    path("clients/", cache_page(60)(ClientListView.as_view()), name="client_list"),
+    path("clients/", cache_page(5)(ClientListView.as_view()), name="client_list"),
     path("clients/create/", ClientCreateView.as_view(), name="create_client"),
     path("clients/view/<int:pk>/", ClientDetailView.as_view(), name="view_client"),
     path("clients/edit/<int:pk>/", ClientUpdateView.as_view(), name="update_client"),
